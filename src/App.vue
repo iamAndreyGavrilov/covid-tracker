17ed51bcd271aef7dd1975d521229a6faf6e758c
Andrey Gavrilov <boombardir20@gmail.com>
2022-01-24 20:38:32 +0500

    add loading img

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,10 @@
 </template>
 
 <script>
-import { fetchCovidData } from "@/api";
 import Header from "@/components/Header.vue";
 export default {
   components: {
     Header,
-  },
-  methods: {
-    async getCovid() {
-      try {
-        const data = await fetchCovidData();
-        console.log(data);
-      } catch (error) {
-        console.error("Ошибка", error);
-      }
-    },
-  },
-  mounted() {
-    this.getCovid();
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="home">hello</div>
+  <main v-if="!loading">Show data</main>
+  <main v-else class="flex flex-col justify-center content-center text-center">
+    <h2 class="text-gray-500 text-3xl mt-10 mb-6">Получение данных</h2>
+    <img :src="loadingImage" alt="loading" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
+import { fetchCovidData } from "@/api";
 export default {
   name: "Home",
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("@/assets/spinner.gif"),
+    };
+  },
   components: {},
+  methods: {
+    async getCovid() {
+      try {
+        const data = await fetchCovidData();
+        this.dataDate = data.Date;
+        this.stats = data.Global;
+        this.countries = data.Countries;
+        this.loading = true;
+      } catch (error) {
+        console.error("Ошибка", error);
+      }
+    },
+  },
+  mounted() {
+    this.getCovid();
+  },
 };
 </script>
